c2560e7ca4008f9cd75b600e4c3b36123b9c3764
jamyspencer <jamy_spencer@hotmail.com>
2017-10-07 21:32:49 -0500

    updated to use string in keywords map

--- resolutions.cpp
+++ resolutions.cpp
@@ -5,13 +5,13 @@
 #include "resolutions.h"
 #include <cstring>
 
-std::unordered_map<char*, bool> keywords = {
+std::unordered_map<std::string, bool> keywords = {
         {"Begin", true}, {"End", true}, {"Check", true}, {"Loop", true}, {"Void", true},
         {"Var", true}, {"Return", true}, {"Input", true}, {"Output", true}, {"Program", true} };
-//static const char keywords[KEYWORD_COUNT][8] = { "Begin", "End", "Check", "Loop", "Void", "Var", "Return", "Input", "Output", "Program" };
 
 void keywordCheck(token* token){
-    if (keywords[token->value]){
+    std::string check = std::string(token->value);
+    if (keywords[check]){
         token->id = KEYWORD_tkn;
         strncpy(token->name, token_names[KEYWORD_tkn], 12);
     }
